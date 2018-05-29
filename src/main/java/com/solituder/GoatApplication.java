099f6ede3906e5891209605b9f31378f1aa75bbd
jwfl724168 <34465021+jwfl724168@users.noreply.github.com>
2018-05-29 11:26:51 +0800

    Initial commit

---
+++ src/main/java/com/solituder/GoatApplication.java
@@ -0,0 +1,12 @@
+package com.solituder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoatApplication.class, args);
+	}
+}
