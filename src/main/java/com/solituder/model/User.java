9408ca624ee644965eb12a710326e1eb1c9f788b
jwfl724168 <34465021+jwfl724168@users.noreply.github.com>
2018-05-29 12:28:37 +0800

    加入user 实体

---
+++ src/main/java/com/solituder/model/User.java
@@ -0,0 +1,46 @@
+package com.solituder.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable
+{
+    private String id;
+    private String name;
+    private Integer age;
+    private Date birthday;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+}
