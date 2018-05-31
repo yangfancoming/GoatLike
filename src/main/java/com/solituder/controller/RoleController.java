cf96f9fefa017781d525a637001820d8629cfc87
jwfl724168 <34465021+jwfl724168@users.noreply.github.com>
2018-05-31 21:58:34 +0800

    增加 role  后台  xml 和 controller

--- src/main/java/com/solituder/controller/RoleController.java
+++ src/main/java/com/solituder/controller/RoleController.java
@@ -18,10 +18,9 @@ import java.util.List;
 import java.util.Map;
 
 
-
 @RestController // 等价于  @Controller +  @ResponseBody
-@RequestMapping(value="/api/sys_user")     // 通过这里配置使下面的映射都在/users下
-public class UserController
+@RequestMapping(value="/api/sys_role")     // 通过这里配置使下面的映射都在/users下
+public class RoleController
 {
     private final ResultGenerator generator = new ResultGenerator();
     @Autowired
@@ -35,57 +34,29 @@ public class UserController
         int pageNum  = (int)map.get("pageNum");
         int pageSize  = (int)map.get("pageSize");
         PageHelper.startPage(pageNum,pageSize);//
-        Page<Map> fuck = baseService.findForList("Sys_userMapper.findForList",map);
+        Page<Map> fuck = baseService.findForList("Sys_roleMapper.findForList",map);
         long total = fuck.getTotal();// 设置返回 总记录数
-        return generator.getSuccessResult("查询用户成功",fuck,total);
-    }
-
-    @RequestMapping(value="/", method= RequestMethod.GET)
-    public List<Map> getUserList()  throws Exception //
-    {
-//        pd.put("tableName","sys_user");// SOS: 这里put的内容必须与 xml中的${} 一一对应啊！ SELECT * FROM ${tableName} WHERE ${columnName} = #{ID}
-        List<Map> fuck =  baseService.findForList("Sys_userMapper.findForList",null);
-        return fuck;
-    }
-    @RequestMapping(value="/{id}", method= RequestMethod.GET)
-    public Map getUser(@PathVariable int id) {
-        Map pd = new HashMap();
-        pd.put("123",id);
-        return pd;
-    }
-    /***
-     *  fuck @RequestParam 表示 请求中 必须带有 名称相同的变量 才能进入该方法 否则直接404 ！！！
-     *  fuck RequestMapping 后的字符串  大小写敏感！！！
-     *  http://localhost:8080/api/sys_user/GET?name=123
-     */
-    @RequestMapping(value="/GET", method= RequestMethod.GET)
-    public String getUser1(@RequestParam String name) {
-        return name;
-    }
-    @RequestMapping(value="user/{id}/{name}",method= RequestMethod.GET)
-    public String myController(@PathVariable String id, @PathVariable String name, ModelMap model)
-    {
-        return "ok";
+        return generator.getSuccessResult("查询角色成功",fuck,total);
     }
 
 
     @RequestMapping(value="/POST", method= RequestMethod.POST)
-    public RestResult POST(HttpServletRequest request) throws  Exception { // sos 这样可以  但是需要注意 这种方式 只能是POST方式 才可以 GET方式 不行！
+    public RestResult POST(HttpServletRequest request) throws  Exception {
         Map map = GoatInfo.getInfo(request);
-        int fuck = baseService.save("Sys_userMapper.save",map);
-        return generator.getSuccessResult("添加用户成功",fuck);
+        int fuck = baseService.save("Sys_roleMapper.save",map);
+        return generator.getSuccessResult("添加角色成功",fuck);
     }
     @RequestMapping(value="/PATCH", method= RequestMethod.POST)
-    public RestResult PATCH(HttpServletRequest request) throws  Exception { // sos  注意： 这里method= RequestMethod.POST 不能使用 PATCH 否则 提示 禁止跨域请求
+    public RestResult PATCH(HttpServletRequest request) throws  Exception {
         Map map = GoatInfo.getInfo(request);
         int fuck = baseService.update("Sys_userMapper.update",map);
-        return generator.getSuccessResult("修改用户成功",fuck);
+        return generator.getSuccessResult("修改角色成功",fuck);
     }
 
     @RequestMapping(value="/DELETE/{id}", method= RequestMethod.DELETE)
     public RestResult DELETE(@PathVariable int id) throws Exception { // sos 已经ok
         int result = baseService.delete("Sys_userMapper.delete",id);
-        return generator.getSuccessResult("删除成功",result);
+        return generator.getSuccessResult("删除角色成功",result);
     }
     @RequestMapping(value="/batchDelete/{ids}", method= RequestMethod.DELETE)
     public RestResult batchDelete(@PathVariable int[] ids) throws Exception{
